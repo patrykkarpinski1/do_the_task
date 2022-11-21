343a283834c8dfc5e1bc910efa65ee1ff5a23226
Patryk Karpinski <patryk.karpinski@interia.pl>
2022-11-21 20:13:15 +0100

    add BottomNavigatorBar to HomePage

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({
     Key? key,
     required this.user,
@@ -10,21 +10,55 @@ class HomePage extends StatelessWidget {
   final User user;
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var currentIndex = 1;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      body: Builder(builder: (context) {
+        if (currentIndex == 1) {
+          return const Center(
+            child: Text('jeden'),
+          );
+        }
+        if (currentIndex == 2) {
+          return const Center(
+            child: Text('dwa'),
+          );
+        }
+        return Center(
           child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('jesteś zalogowany jako ${user.email}'),
-          ElevatedButton(
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
-            },
-            child: const Text('Wyloguj się'),
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('jesteś zalogowany jako ${widget.user.email}'),
+              ElevatedButton(
+                onPressed: () {
+                  FirebaseAuth.instance.signOut();
+                },
+                child: const Text('Wyloguj się'),
+              ),
+            ],
           ),
+        );
+      }),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (newIndex) {
+          setState(() {
+            currentIndex = newIndex;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person), label: 'Moje konto'),
+          BottomNavigationBarItem(icon: Icon(Icons.check), label: 'Zadania'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings), label: 'Ustawienia'),
         ],
-      )),
+      ),
     );
   }
 }
