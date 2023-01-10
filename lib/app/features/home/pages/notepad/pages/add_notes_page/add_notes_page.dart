b2716bd42b528b5f441ac4e2351b204c25ba5c33
Patryk Karpinski <patryk.karpinski@interia.pl>
2023-01-10 18:05:39 +0100

    Pop add note screen on save

--- lib/app/features/home/pages/notepad/pages/add_notes_page/add_notes_page.dart
+++ lib/app/features/home/pages/notepad/pages/add_notes_page/add_notes_page.dart
@@ -12,68 +12,75 @@ class AddNotes extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => AddNoteCubit()..start(),
-      child: BlocBuilder<AddNoteCubit, AddNoteState>(
-        builder: (context, state) {
-          return StreamBuilder<Object>(
-              stream: null,
-              builder: (context, snapshot) {
-                return Scaffold(
-                  backgroundColor: const Color.fromARGB(255, 49, 171, 175),
-                  appBar: AppBar(
-                    leading: IconButton(
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                      icon: const Icon(
-                        Icons.arrow_back,
-                        color: Color.fromARGB(255, 247, 143, 15),
-                      ),
-                    ),
-                    actions: [
-                      IconButton(
-                        onPressed: state.textNote.isEmpty
-                            ? null
-                            : () {
-                                context
-                                    .read<AddNoteCubit>()
-                                    .add(state.textNote);
-                              },
+      child: BlocListener<AddNoteCubit, AddNoteState>(
+        listener: (context, state) {
+          if (state.saved) {
+            Navigator.of(context).pop();
+          }
+        },
+        child: BlocBuilder<AddNoteCubit, AddNoteState>(
+          builder: (context, state) {
+            return StreamBuilder<Object>(
+                stream: null,
+                builder: (context, snapshot) {
+                  return Scaffold(
+                    backgroundColor: const Color.fromARGB(255, 49, 171, 175),
+                    appBar: AppBar(
+                      leading: IconButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
                         icon: const Icon(
-                          Icons.check,
+                          Icons.arrow_back,
                           color: Color.fromARGB(255, 247, 143, 15),
                         ),
                       ),
-                    ],
-                    backgroundColor: const Color.fromARGB(255, 1, 100, 146),
-                    title: Text(
-                      'NOTE',
-                      style: GoogleFonts.rubikBeastly(
-                        color: const Color.fromARGB(255, 247, 143, 15),
+                      actions: [
+                        IconButton(
+                          onPressed: state.textNote.isEmpty
+                              ? null
+                              : () {
+                                  context
+                                      .read<AddNoteCubit>()
+                                      .add(state.textNote);
+                                },
+                          icon: const Icon(
+                            Icons.check,
+                            color: Color.fromARGB(255, 247, 143, 15),
+                          ),
+                        ),
+                      ],
+                      backgroundColor: const Color.fromARGB(255, 1, 100, 146),
+                      title: Text(
+                        'NOTE',
+                        style: GoogleFonts.rubikBeastly(
+                          color: const Color.fromARGB(255, 247, 143, 15),
+                        ),
                       ),
                     ),
-                  ),
-                  body: ListView(
-                    padding: const EdgeInsets.all(20),
-                    children: [
-                      TextField(
-                        decoration: const InputDecoration(
-                            hintText: "Your note",
-                            border: OutlineInputBorder(),
-                            filled: true,
-                            fillColor: Color.fromARGB(255, 133, 220, 223)),
-                        maxLength: 10000,
-                        maxLines: 30,
-                        onChanged: (newTextNote) {
-                          context
-                              .read<AddNoteCubit>()
-                              .changetextNote(newTextNote);
-                        },
-                      ),
-                    ],
-                  ),
-                );
-              });
-        },
+                    body: ListView(
+                      padding: const EdgeInsets.all(20),
+                      children: [
+                        TextField(
+                          decoration: const InputDecoration(
+                              hintText: "Your note",
+                              border: OutlineInputBorder(),
+                              filled: true,
+                              fillColor: Color.fromARGB(255, 133, 220, 223)),
+                          maxLength: 10000,
+                          maxLines: 30,
+                          onChanged: (newTextNote) {
+                            context
+                                .read<AddNoteCubit>()
+                                .changetextNote(newTextNote);
+                          },
+                        ),
+                      ],
+                    ),
+                  );
+                });
+          },
+        ),
       ),
     );
   }
